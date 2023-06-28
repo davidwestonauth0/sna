864311d0a73ef95b125c637da1d8421bb0661151
David Weston <david.weston@auth0.com>
2023-06-28 13:24:45 +0100

    fix

--- index.js
+++ index.js
@@ -172,13 +172,6 @@ async function hitSna(url) {
 //var response = hitSna("<%= fields.sna_url %>");
 //console.log(response);
 
-//const request = new XMLHttpRequest();
-//request.open("GET", "<%= fields.sna_url %>", false); // `false` makes the request synchronous
-//request.send(null);
-//
-//if (request.status === 200) {
-//  console.log(request.responseText);
-}
 //window.location.replace = "<%= fields.sna_url %>";
 
     const xhr = new XMLHttpRequest();
